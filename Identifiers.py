6595bcb0e4971f13b7250ca1553748e95eb5e351
VangapanduLakshmi <135969667+VangapanduLakshmi@users.noreply.github.com>
2023-10-23 18:17:05 +0530

    Add files via upload

---
+++ Identifiers.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # **Identifiers**
+
+# **Definition:** Giving a name to value/entity.
+# it can be **function name(or), class name(or), madule name(or), object name(or), variable name.**
+
+# Examples:
+#     name="Lakshmi"
+#     batch=241
+#     cource_name= " data science"
+#     percentage= 92.5
+#     
+
+# **Rules**
+#     
+
+# **Do's:**
+# 
+# ---> Identifiers contains alphanumeric values and Underscore(_) only.
+#     
+#     ex: a-z, A-Z, 0-9, _
+#         
+# ---> it should be start with characters and underscore(_).
+# 
+# ---> we can also creat the identifier with combination of alphanumeric values and underscore(_). but it should be start with characters and underscore(_).  
+#     
+#     
+
+# **Examples**
+
+# In[4]:
+
+
+value=10
+
+
+# In[6]:
+
+
+marks=200
+
+
+# In[7]:
+
+
+name1="laxmi"
+
+
+# In[8]:
+
+
+name2="lakshmi"
+
+
+# In[9]:
+
+
+_per=23.4
+
+
+# In[10]:
+
+
+game_name="cricket"
+
+
+# In[1]:
+
+
+name="lakshmi"
+
+
+# In[2]:
+
+
+name
+
+
+# **don'ts:**
+# 
+# ----> Identifier shoud not start with numbers.
+# 
+# ----> we can't use any special symblos, except underscore(_).
+# 
+# ----> we can't use key words
+#      
+
+# **Examples**
+
+# In[11]:
+
+
+1name="lakshmi" 
+
+
+# In[12]:
+
+
+2value=130
+
+
+# In[15]:
+
+
+name@12="innomatics"
+
+
+# In[49]:
+
+
+import keyword
+keyword.kwlist
+
+
+# In[20]:
+
+
+if="innomatics"
+
+
+# In[21]:
+
+
+If="innomatics"
+
+
+# In[22]:
+
+
+If
+
+
+# In[23]:
+
+
+false= 100
+
+
+# In[24]:
+
+
+False =100
+
+
+# In[25]:
+
+
+continue = "ok"
+
+
+# In[26]:
+
+
+Continue="ok"
+
+
+# In[27]:
+
+
+Continue
+
+
+# **There is length limit for python identifiers**
+
+# In[43]:
+
+
+hbcjsdhbcjxjdshgfiuwesnckHDVFICEWDXBASJCVSUGFWEHDCLBJHXBCJHDGFILUHEARKFVBFDKFGTNVEDBCJVDFNVHBFhciasbdcsbagverbdsxxdkdfBjfdbvjjfvbjbfffjvbz=100
+
+
+# In[44]:
+
+
+hbcjsdhbcjxjdshgfiuwesnckHDVFICEWDXBASJCVSUGFWEHDCLBJHXBCJHDGFILUHEARKFVBFDKFGTNVEDBCJVDFNVHBFhciasbdcsbagverbdsxxdkdfBjfdbvjjfvbjbfffjvbz
+
+
+# **Identofiers are case senstive**
+
+# In[45]:
+
+
+name="innomatics"
+
+
+# In[46]:
+
+
+name
+
+
+# In[47]:
+
+
+NAME
+
+
+# In[ ]:
+
+
+
+
